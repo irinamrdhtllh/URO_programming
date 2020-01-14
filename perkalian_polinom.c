e1da79985afc856d595ea4d2160758f5497e3be6
Irina M. S <52401808+irinamrdhtllh@users.noreply.github.com>
2020-01-14 13:43:13 +0700

    Update perkalian_polinom.c

--- perkalian_polinom.c
+++ perkalian_polinom.c
@@ -4,64 +4,61 @@ void printPolynom(int poly[], int num);
 
 int main()
 {
-    int a, b, c, i, j, n, m;
-    int f[100], g[100], result[100];
+	int a, b, c, i, j, n, m;
+    	int f[100], g[100], result[100];
 
-    printf("Masukkan derajat tertinggi polinom f (n-1) : ");
-    scanf("%d", &n);
-    for (i = 0; i < n; ++i){
-        printf("Masukkan koefisien x^%d: ", i);
-        scanf("%d", &f[i]);
-    }
+    	printf("Masukkan derajat tertinggi polinom f: ");
+    	scanf("%d", &n);
+    	for (i = 0; i <= n; ++i){
+        	printf("Masukkan koefisien x^%d: ", i);
+        	scanf("%d", &f[i]);
+   	}
 
-    printf("Polinom pertama adalah: ");
-    printPolynom(f, n);
+    	printf("Polinom pertama adalah: ");
+    	printPolynom(f, n);
 
-    printf("\nMasukkan derajat tertinggi polinom g (m-1): ");
-    scanf("%d", &m);
-    for (j = 0; j < m; ++j){
-        printf("Masukkan koefisien x^%d: ", j);
-        scanf("%d", &g[j]);
-    }
+    	printf("\nMasukkan derajat tertinggi polinom g: ");
+    	scanf("%d", &m);
+    	for (j = 0; j <= m; ++j){
+        	printf("Masukkan koefisien x^%d: ", j);
+        	scanf("%d", &g[j]);
+    	}
 
-    printf("Polinom kedua adalah: ");
-    printPolynom(g, m);
+    	printf("Polinom kedua adalah: ");
+    	printPolynom(g, m);
 
-    for (c = 0; c < (n+m-1); ++c){
-        result[c] = 0;
-    }
+    	for (c = 0; c <= (n+m); ++c){
+        	result[c] = 0;
+    	}
 
-    for (a = 0; a < n; ++a){
-        for (b = 0; b < m; ++b){
-            result[a+b] += f[a]*g[b];
-        }
-    }
+    	for (a = 0; a <= n; ++a){
+        	for (b = 0; b <= m; ++b){
+            		result[a+b] += f[a]*g[b];
+        	}
+   	}
 
-    printf("\nHasil perkalian kedua polinom adalah: ");
-    printPolynom(result, (m+n-1));
+    	printf("\nHasil perkalian kedua polinom adalah: ");
+    	printPolynom(result, (m+n));
 
-    return 0;
+    	return 0;
 }
 
 void printPolynom(int poly[], int num)
 {
-    int k;
+	int k;
 
-    for (k = 0; k < num; ++k){
-    	if (poly[k] != 0){
-        printf("%d", poly[k]);
-    	}
-    	else if (poly[k] == 0){
-    		continue;
+    	for (k = 0; k <= num; ++k){
+		printf("%d", poly[k]);
+		if (k != 0){
+			if (k == 1){
+				printf("x");
+			}
+			else{
+				printf("x^%d", k);
+			}
 		}
-		if (k == 1){
-        	printf("x");
-    	}
-        else if (k != 0){
-            printf("x^%d", k);
-        }
-        if (k < (num - 1)){
-            printf(" + ");
-        }
-    }
+		if (k < num){
+			printf(" + ");
+		}
+	}		
 }
